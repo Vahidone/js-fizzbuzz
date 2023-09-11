68a09cdf1ee8856d184c279398e04d6c48f18a7a
Vahidone <v.ban.roma@gmail.com>
2023-09-11 17:03:00 +0200

    feat: add js file

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+// prendiamo la classe del contenitore dei box 
+const boxContainer = document.querySelector('.box-container');
+// aggiungiamo il titolo 
+boxContainer.innerHTML += ` <h1>FizzBuzzDOM</h1>`;
+
+
+// usiamo il ciclo For 
+for (let i = 1; i <= 100; i++){
+    // creiamo il div per i box 
+    const box = document.createElement('div');
+    // diamo una classe ai nostri box 
+    box.classList.add('box');
+  
+    // usare "if" per creare condizioni
+    if ((!(i % 3)) && (!(i % 5))) {
+      box.append('FizzBuzz')
+      box.classList.add('box-fizzbuzz');
+    } else if (!(i % 3)) {
+      box.append('Fizz');
+      box.classList.add('box-fizz'); 
+    } else if (!(i % 5)) {
+      box.append('Buzz');
+      box.classList.add('box-buzz');
+    }
+    else {
+      box.append(i);
+    }
+
+  boxContainer.append(box);
+}
